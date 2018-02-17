d715de5d341f3f295b6430b7fb7372118dc1b193
MAYANK BANSAL <31138914+mkblgnj@users.noreply.github.com>
2018-02-17 14:51:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,20 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New value';
+var button = document.getElementById('counter');
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft+'px';
+button.onclick = function() {
     
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    
+    request.open('GET','http://mkblgnj.imad.hasura-app.io/',true);
+    request.send(null);
 }
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-};
