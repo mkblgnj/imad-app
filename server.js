cbe7e023d7f92261ac68c4f0911038c60ad591f6
MAYANK BANSAL <31138914+mkblgnj@users.noreply.github.com>
2018-02-17 21:14:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,13 @@ app.get('/counter', function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
+var names = [];
+app.get('/submit-name', function(req, res) {
+    var name = req.query.name;
+    
+    names.send(JSON.stringify(names));
+    
+});
 
 app.get('/:articleName',function(req,res) {
     var articleName = req.params.articleName;
