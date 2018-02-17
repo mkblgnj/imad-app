d6f834d1bad893ee6e9a4ee7fc7ae746a9b66b99
MAYANK BANSAL <31138914+mkblgnj@users.noreply.github.com>
2018-02-17 14:22:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,11 @@ app.get('/:articleName',function(req,res) {
     var articleName = req.params.articleName;
      res.send(createTemplate(articles[articleName]));
 });
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 
 
 app.get('/ui/style.css', function (req, res) {
