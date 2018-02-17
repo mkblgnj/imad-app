5ab0bda6a09ecdd8b8c9281fd4d4ebab2d8b5760
MAYANK BANSAL <31138914+mkblgnj@users.noreply.github.com>
2018-02-17 21:41:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ submit.onclick = function () {
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     
-    request.open('GET','http://mkblgnj.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://mkblgnj.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
    
